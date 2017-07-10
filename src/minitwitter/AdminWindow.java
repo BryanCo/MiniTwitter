a639609493d1b6fda83598076c7e28e7cc715a80
Bryan <Bryan@Bryan>
2017-07-09 17:45:00 -0700

    Cleanup Code.

--- src/minitwitter/AdminWindow.java
+++ src/minitwitter/AdminWindow.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package minitwitter;
 
 import java.awt.Dimension;
@@ -12,11 +7,6 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextField;
-import javax.swing.JTree;
-import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.TreeSelectionModel;
 
 /**
  *
